509dc855b773d38e8f4c8e3c86eb298e00b7dce9
ksahirani <kenonsahirani07@gmail.com>
2026-01-01 19:49:00 +0800

    Added Exercise 2

---
+++ practiceExercise2.js
@@ -0,0 +1,8 @@
+// Exercise 2: Simple Calculator
+const numberOne = 32;
+const numberTwo = 20;
+
+console.log(`${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`);
+console.log(`${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`);
+console.log(`${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`);
+console.log(`${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`);
