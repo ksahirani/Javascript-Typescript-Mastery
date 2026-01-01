153756f76936f91a4b83bdd3ff8fc6084b47f6e7
ksahirani <kenonsahirani07@gmail.com>
2026-01-01 21:43:40 +0800

    Slight changes in Exercise 3: Product Object

--- practiceExercise3.js
+++ practiceExercise3.js
@@ -1,9 +1,12 @@
 // Practice Exercise 3: Product Object
 const product = {
     name: "Planner",
-    price: "₱299.99",
+    price: 299.99,
     stocks: 50,
     isAvailable: true,
     categories: ["Weekly", "Monthly", "Yearly"],
 }
-console.log(`The ${product.name} cost ${product.price} and we have ${product.stocks} in stock`);
+console.log(`The ${product.name} cost ₱${product.price} and we have ${product.stocks} in stock`);
+
+// Slight changes in price, from string to number
+// Best practice price as a number not a string
