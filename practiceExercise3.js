fdd456183d56ac36fd52ea29801da0ddc1ae0e27
ksahirani <kenonsahirani07@gmail.com>
2026-01-01 21:22:28 +0800

    Added Exercise 3: Product Object

---
+++ practiceExercise3.js
@@ -0,0 +1,9 @@
+// Practice Exercise 3: Product Object
+const product = {
+    name: "Planner",
+    price: "₱299.99",
+    stocks: 50,
+    isAvailable: true,
+    categories: ["Weekly", "Monthly", "Yearly"],
+}
+console.log(`The ${product.name} cost ${product.price} and we have ${product.stocks} in stock`);
