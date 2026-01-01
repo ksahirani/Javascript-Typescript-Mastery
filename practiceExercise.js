e9b56efd71d6aec13bf493f99aa44e11cf79d9f0
ksahirani <kenonsahirani07@gmail.com>
2026-01-01 18:55:05 +0800

    Added Exercise 1 file

---
+++ practiceExercise.js
@@ -0,0 +1,18 @@
+// Practice Exercise 1: Create a profile
+const fullName = "Kenon Macabada Sahirani";
+const age = 32;
+const isDeveloper = true;
+const skills = ["Javascript/Typescript", "Express", "React", "Java", "Springboot", "C#", "ASP.Net Core", "SQL", "PostgreSQL"];
+const info = {
+        name: "Kenon Sahirani",
+        country: "Philippines",
+        frontend: "HTML, CSS, Tailwind",
+        backend: "Java, C#, Javascript, Ruby, Python",
+        database: "postgresql, mysql, mongodb"
+}
+
+console.log("Name:", fullName);
+console.log("Age:", age);
+console.log("Is Developer:", isDeveloper);
+console.log("Skills:", skills);
+console.log("Profile:", info);
