1ddfd8ddb96b158361963414929ab67f979bd825
ksahirani <kenonsahirani07@gmail.com>
2026-01-02 16:43:36 +0800

    Added the javascript comparisons

---
+++ jsOperations&Conditionals.js
@@ -0,0 +1,33 @@
+// Lesson 2: Operators and Conditionals
+
+/*
+1. Comparison operators - comparing values
+2. Logical operators - combining conditions
+3. if/else statements - making decisions
+4. Ternary operator - shortcut for if/else
+5. Switch statements - multiple choices 
+ */
+
+// 1. Comparison Operator
+/* 
+Basic Comparisons:
+1. Equal to (strict) - (===) - 5 === 5 - true
+2. Not equal to (strict)- (!==) - 5 !== 3 - true
+3. Greater than - (>) - 10 > 5 - true
+4. Less than - (<) - 3 < 7 - true
+5. Greater than or equal - (>=) - 5 >= 5 - true
+6. Less than or equal - (<=) - 4 <= 4 - true
+*/
+
+const age = 25;
+const price = 299.99;
+const stock = 0;
+
+console.log(age === 25); // true
+console.log(age === "25"); //false
+console.log(age !== 30); // true
+console.log(price > 200); // true
+console.log(stock <= 0); //true
+console.log(age >= 18); //true (adult check)
+
+
