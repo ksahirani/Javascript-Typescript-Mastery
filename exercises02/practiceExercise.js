e244428b0c032cc6956126ad6fa8c1f740073341
ksahirani <kenonsahirani07@gmail.com>
2026-01-09 02:55:26 +0800

    Added the exercise02 practiceexercise

---
+++ exercises02/practiceExercise.js
@@ -0,0 +1,11 @@
+const age = 32;
+
+if (age < 13) {
+    console.log("Child");
+} else if (age >= 13 && age <= 17) {
+    console.log("Teenager");
+} else if (age >= 18 && age <= 59) {
+    console.log("Adult")
+} else {
+    console.log("Senior");
+}
